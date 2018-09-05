94003158be5b70383ad09fc7f8e605e3cd5fb182
Biel Simon Rojas <bielsimon2@gmail.com>
2018-09-05 12:06:45 +0200

    Added path notation support

--- src/namespaces.test.tsx
+++ src/namespaces.test.tsx
@@ -32,4 +32,36 @@ describe('namespaces', function () {
             </div>
         </Namespace>)
     });
+    it('absolute path', function () {
+        renderer.create(<Namespace name={'level1'}>
+            <div>
+                <Namespace name={'/level2'}>
+                    <div>
+                        <Namespace>
+                            {(level) => {
+                                expect(level).toEqual(['level2'])
+                                return null
+                            }}
+                        </Namespace>
+                    </div>
+                </Namespace>
+            </div>
+        </Namespace>)
+    });
+    it('relative path', function () {
+        renderer.create(<Namespace name={'level1'}>
+            <div>
+                <Namespace name={'level2'}>
+                    <Namespace name={'../level3'}>
+                        <Namespace>
+                            {(level) => {
+                                expect(level).toEqual(['level1', 'level3'])
+                                return null
+                            }}
+                        </Namespace>
+                    </Namespace>
+                </Namespace>
+            </div>
+        </Namespace>)
+    });
 });
--- src/namespaces.tsx
+++ src/namespaces.tsx
@@ -4,32 +4,47 @@ export type NamespaceContext = string[];
 
 const {Provider: NamespaceProvider, Consumer: NamespaceConsumer} = React.createContext<NamespaceContext>([]);
 
-export const Namespace = (props: {name?: string, children: any}) => {
-    if((props as any).name == null){
-        const cprops = (props as {children: any})
+export const Namespace = (props: { name?: string, children: any }) => {
+    // Read
+    if ((props as any).name == null) {
+        const cprops = (props as { children: any })
         return <NamespaceConsumer>
             {(namespace) => {
                 // console.log('namespace', namespace);
-                if(typeof cprops.children == "function") return cprops.children(namespace)
+                if (typeof cprops.children == "function") return cprops.children(namespace)
                 return cprops.children
             }}
         </NamespaceConsumer>
     }
+    // Write
     return <NamespaceConsumer>
-        {currentNamespace => {
-            const rprops = (props as {name: string, children: any})
-            return <NamespaceProvider value={currentNamespace.concat(rprops.name)}>
+        {(currentNamespace: string[]) => {
+            const rprops = (props as { name: string, children: any })
+            let newName = rprops.name;
+            const parts = newName.split('/')
+            parts.forEach((part, i) => {
+                if (part === '' && i === 0) {
+                    currentNamespace = [];
+                    return
+                }
+                if (part === '..') {
+                    currentNamespace.pop();
+                    return
+                }
+                currentNamespace = currentNamespace.concat(part);
+            })
+            return <NamespaceProvider value={currentNamespace}>
                 {props.children}
             </NamespaceProvider>
         }}
-    </NamespaceConsumer>
-}
-export const PrintNamespace = (props: {}) => {
-    return <Namespace>
+            </NamespaceConsumer>
+        }
+        export const PrintNamespace = (props: {}) => {
+        return <Namespace>
         {(namespace) => {
             return <ol>
                 {namespace.join('.')}
             </ol>
         }}
-    </Namespace>
-}
+        </Namespace>
+    }
